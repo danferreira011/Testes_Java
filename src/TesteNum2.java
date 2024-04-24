fad9a72c88c1b3480673f5f12bf6f3abccde630a
Danilo Ferreira <danilosantos.turbo@gmail.com>
2024-04-24 10:01:59 -0300

    Testes em java

---
+++ src/TesteNum2.java
@@ -0,0 +1,11 @@
+public class TesteNum2{
+    public static void main(String[] args) {
+        
+        double myDouble =9.78d;
+        int myInt=(int) myDouble;
+
+        //System.out.println('\n');
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
