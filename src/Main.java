c6da7979bbd1a22a544990e3598c0f98b0bbbfc9
Danilo Ferreira <danilosantos.turbo@gmail.com>
2024-04-24 13:45:45 -0300

    +testes

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    int x=5;
+   
+    public static void main(String[] args){
+        Main objeto=new Main();
+        
+        //objeto.x=40;
+        System.out.println("Resultado = "+objeto.x);
+    }
+}
